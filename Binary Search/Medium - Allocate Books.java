e580e4f4e16ff0a2a0454d3b68d89d709e47bfdc
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-08-17 01:42:56 +0530

    Update Medium - Allocate Books.java

--- Binary Search/Medium - Allocate Books.java
+++ Binary Search/Medium - Allocate Books.java
@@ -1,2 +1,48 @@
 TC: O(N logN);
 SC: O(1);
+
+
+public class Solution{
+    public int books(int[] A, int B){
+        if(B > A.length){
+            return -1;
+        }
+        int high = 0; int low = 0; int ans = -1;
+        for(int i=0; i<A.length; i++){
+            high += A[i];
+            low = Math.min(low, A[i]);
+        }
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(allocationIsPossible(mid)){
+                ans = mid;
+                high = mid -1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    boolean allocationIsPoosible(int barrier){
+        int allocated_student = 1;
+        int pages = 0;
+
+        for(int i=0; i<A.length; i++){
+            if(A[i] > barrier){
+                return false;
+            }
+            if(pages + A[i] > barrier){
+                allocated_student += 1;
+                pages += A[i];
+            }
+            else{
+                pages += A[i];
+            }
+        } 
+        if (allocated_student > B){
+            return false;
+        }
+        return true;
+    }
+}
